ca197cd4e9c8b96f83aa0e58576d8ab2dc98d468
colldata16 <79728859+colldata16@users.noreply.github.com>
2021-02-28 19:33:48 -0500

    Update Archivo1.R

---
+++ Archivo1.R
@@ -0,0 +1,11 @@
+install.packages("tidyverse", dependencies = TRUE)
+
+install.packages("broom")
+
+library(tidyverse)
+
+install.packages("magic", dependencies = TRUE)
+
+library(magic)
+
+magic(6)
